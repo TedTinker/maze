95ac85107122fae92c5032532346ff844aa43136
TedTinker <tedjtinker@gmail.com>
2023-04-09 13:24:24 +0900

    added zp

--- agent.py
+++ agent.py
@@ -162,15 +162,13 @@ class Agent:
 
         # Train forward
         if(self.args.state_forward):
-            pred_obs = [] ; obs_mus_b = [] ; obs_stds_b = [] ; zp_mus = [] ; zp_stds = [] ; zq_mus = [] ; zq_stds = [] ; h = None
+            pred_obs = [] ; obs_mus_b = [] ; obs_stds_b = [] ; zq_mus = [] ; zq_stds = [] ; h = None
             for step in range(steps):
-                p_obs, (obs_mu, obs_std), (zp_mu, zp_std), (zq_mu, zq_std), h = self.forward(obs[:, step], prev_actions[:, step], actions[:, step], h)   
+                p_obs, (obs_mu, obs_std), _, (zq_mu, zq_std), h = self.forward(obs[:, step], prev_actions[:, step], actions[:, step], h)   
                 pred_obs.append(p_obs) ; obs_mus_b.append(obs_mu) ; obs_stds_b.append(obs_std)
-                zp_mus.append(zp_mu) ; zp_stds.append(zp_std)
                 zq_mus.append(zq_mu) ; zq_stds.append(zq_std)
             pred_obs = torch.cat(pred_obs, dim = 1)
             obs_mus_b = torch.cat(obs_mus_b, dim = 1) ; obs_stds_b = torch.cat(obs_stds_b, dim = 1)
-            zp_mus = torch.cat(zp_mus, dim = 1) ; zp_stds = torch.cat(zp_stds, dim = 1)
             zq_mus = torch.cat(zq_mus, dim = 1) ; zq_stds = torch.cat(zq_stds, dim = 1)
             if(self.args.accuracy == "mse"):      accuracy = F.mse_loss(pred_obs, next_obs, reduction = "none").sum(-1).unsqueeze(-1)
             if(self.args.accuracy == "log_prob"): accuracy = 0.5 * (torch.log(2 * np.pi * obs_stds_b**2) + ((next_obs - obs_mus_b) ** 2) / (obs_stds_b**2 + 1e-6)).sum(-1).unsqueeze(-1)
@@ -188,6 +186,19 @@ class Agent:
             self.forward_opt.zero_grad()
             forward_loss.backward()
             self.forward_opt.step()
+            
+            zp_mus = [] ; zp_stds = [] ; zq_mus = [] ; zq_stds = [] ; h = None
+            for step in range(steps):
+                _, _, (zp_mu, zp_std), (zq_mu, zq_std), h = self.forward(obs[:, step], prev_actions[:, step], actions[:, step], h)   
+                zp_mus.append(zp_mu) ; zp_stds.append(zp_std)
+                zq_mus.append(zq_mu) ; zq_stds.append(zq_std)
+            zp_mus = torch.cat(zp_mus, dim = 1) ; zp_stds = torch.cat(zp_stds, dim = 1)
+            zq_mus = torch.cat(zq_mus, dim = 1) ; zq_stds = torch.cat(zq_stds, dim = 1)
+            zp_loss = dkl(zp_mus,  zp_stds, zq_mus,  zq_stds).mean()
+            
+            self.zp_opt.zero_grad()
+            zp_loss.backward()
+            self.zp_opt.step()
         else:
             pred_obs, (obs_mus_b, obs_stds_b) = self.forward(obs, prev_actions, actions)   
             if(self.args.accuracy == "mse"):      accuracy = F.mse_loss(pred_obs, next_obs, reduction = "none").sum(-1).unsqueeze(-1)
