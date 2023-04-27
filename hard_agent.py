83e2633efdebe265796324a1614d622bbbe7d414
TedTinker <tedjtinker@gmail.com>
2023-04-27 12:07:29 +0900

    Changed dkl_max

--- hard_agent.py
+++ hard_agent.py
@@ -334,7 +334,9 @@ class Agent:
         
         # Get curiosity                  
         naive_curiosity = self.args.naive_eta * accuracy_for_naive  
-        free_curiosity  = self.args.free_eta  * torch.clamp(complexity_for_free, min = 0, max = self.args.dkl_max)
+        if(self.args.dkl_max != None):
+            complexity_for_free = torch.clamp(complexity_for_free, min = 0, max = self.args.dkl_max)
+        free_curiosity = self.args.free_eta * complexity_for_free
         if(self.args.curiosity == "naive"):  curiosity = naive_curiosity
         elif(self.args.curiosity == "free"): curiosity = free_curiosity
         else:                                curiosity = torch.zeros(rewards.shape)
